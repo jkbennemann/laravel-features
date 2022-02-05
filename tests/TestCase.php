310baf3f40b6ed455bd8d66caec7758881afdd09
Jakob Bennemann <jakob@raidboxes.de>
2022-02-05 09:43:39 +0100

    add migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,25 +12,45 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
+        $this->setUpDatabase();
+
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Jkbennemann\\Features\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            FeaturesServiceProvider::class,
         ];
+    }
+
+    protected function setUpDatabase(): void
+    {
+        $this->loadLaravelMigrations();
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_feature_table.php.stub';
         $migration->up();
-        */
+
+        $migration = include __DIR__.'/../database/migrations/create_party_table.php.stub';
+        $migration->up();
+
+        $migration = include __DIR__.'/../database/migrations/create_feature_party_table.php.stub';
+        $migration->up();
+
+        $migration = include __DIR__.'/../database/migrations/create_feature_user_table.php.stub';
+        $migration->up();
+
+        $migration = include __DIR__.'/../database/migrations/create_party_user_table.php.stub';
+        $migration->up();
     }
 }
