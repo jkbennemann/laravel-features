45e45ff2fd592a2c579403a61b4bf0d2e8932457
Jakob Bennemann <jakob@raidboxes.de>
2022-02-05 13:20:06 +0100

    add commands to list party/feature

---
+++ src/Commands/FeatureListCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Jkbennemann\Features\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\QueryException;
+use Jkbennemann\Features\Models\Enums\FeatureStatus;
+use Jkbennemann\Features\Models\Feature;
+
+class FeatureListCommand extends Command
+{
+    public $signature = 'features:list';
+
+    public $description = 'List all features';
+
+    public function handle(): int
+    {
+       $features = Feature::all();
+
+       $this->table(['ID', 'Name', 'Slug', 'Status', 'Description', 'Created'], $features->map(function(Feature $feature) {
+           return [
+               $feature->id,
+               $feature->name,
+               $feature->slug,
+               FeatureStatus::getLabel($feature->status),
+               $feature->description,
+               $feature->created_at->format('d.m.Y H:i:s (T)')
+           ];
+       }));
+
+        return self::SUCCESS;
+    }
+
+    private function parseStatus(string $status = null): int
+    {
+        if (!is_numeric($status) && !empty($status)) {
+            $this->error('Status has to be one of [0,1,2,3]');
+        }
+
+        return match ((int)$status) {
+            1 => FeatureStatus::ACTIVE,
+            2 => FeatureStatus::PLANNED,
+            3 => FeatureStatus::ABANDONED,
+            default => FeatureStatus::INACTIVE,
+        };
+    }
+}
---
+++ src/Commands/PartyListCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Jkbennemann\Features\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Database\QueryException;
+use Jkbennemann\Features\Models\Enums\FeatureStatus;
+use Jkbennemann\Features\Models\Enums\PartyStatus;
+use Jkbennemann\Features\Models\Feature;
+use Jkbennemann\Features\Models\Party;
+
+class PartyListCommand extends Command
+{
+    public $signature = 'party:list';
+
+    public $description = 'List all parties';
+
+    public function handle(): int
+    {
+       $parties = Party::all();
+
+       $this->table(['ID', 'Name', 'Slug', 'Status', 'Description', 'Created'], $parties->map(function(Party $party
+       ) {
+           return [
+               $party->id,
+               $party->name,
+               $party->slug,
+               PartyStatus::getLabel($party->status),
+               $party->description,
+               $party->created_at->format('d.m.Y H:i:s (T)')
+           ];
+       }));
+
+        return self::SUCCESS;
+    }
+
+    private function parseStatus(string $status = null): int
+    {
+        if (!is_numeric($status) && !empty($status)) {
+            $this->error('Status has to be one of [0,1,2,3]');
+        }
+
+        return match ((int)$status) {
+            1 => FeatureStatus::ACTIVE,
+            2 => FeatureStatus::PLANNED,
+            3 => FeatureStatus::ABANDONED,
+            default => FeatureStatus::INACTIVE,
+        };
+    }
+}
