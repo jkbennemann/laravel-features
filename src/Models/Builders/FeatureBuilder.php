5c16bc1718336b549e97f611ff44e2619f6657df
jkbennemann <jkbennemann@users.noreply.github.com>
2022-02-06 12:09:20 +0000

    Fix styling

--- src/Models/Builders/FeatureBuilder.php
+++ src/Models/Builders/FeatureBuilder.php
@@ -3,8 +3,6 @@
 namespace Jkbennemann\Features\Models\Builders;
 
 use Illuminate\Database\Eloquent\Builder;
-use Jkbennemann\Features\Models\Builders\Traits\HasByName;
-use Jkbennemann\Features\Models\Builders\Traits\HasBySlug;
 use Jkbennemann\Features\Models\Builders\Traits\HasByStatus;
 
 class FeatureBuilder extends Builder
